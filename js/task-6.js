3498b64e04f557883546494121ac373a1ce70cb6
Yelenay Uçar <yelenayucr@gmail.com>
2024-12-15 15:39:08 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,26 +1,34 @@
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
-const buttons = document.querySelectorAll('button[type="button"]');
-document.querySelector('[data-create]').addEventListener('click', () =>{
+
+function createBoxes(amount) {
+    const boxes = document.getElementById('boxes');
+    boxes.innerHTML = ''; 
+
+    for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div'); 
+        const size = 30 + i * 10;
+
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.style.backgroundColor = getRandomHexColor();
+
+        boxes.appendChild(box);
+    }
+}
+
+document.querySelector('[data-create]').addEventListener('click', () => {
     const amount = +document.querySelector('input[type="number"]').value;
     if (amount >= 1 && amount <= 100) {
-        const boxes = document.getElementById('boxes');
-        boxes.innerHTML = '';
-        for (let i = 0; i < amount; i++){
-            const box = document.createElement('div');
-            box.style.width = `${30 + i * 10}px`;
-            box.style.height = `${30 + i * 10}px`;
-            box.style.backgroundColor = getRandomHexColor();
-            boxes.appendChild(box);
-        }
+        createBoxes(amount);
     }
     document.querySelector('input[type="number"]').value = '';
 });
 
 document.querySelector('[data-destroy]').addEventListener('click', () => {
-    document.getElementById('boxes').innerHTML = ''; 
-    
-}); 
+    document.getElementById('boxes').innerHTML = '';
+});
